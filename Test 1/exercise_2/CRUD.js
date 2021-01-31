d867f4e6fd25f5b721e2e8a015878cb982653c45
Hung Nguyen <hpn2@rice.edu>
2021-01-31 21:45:50 +0700

    done

--- Test 1/exercise_2/CRUD.js
+++ Test 1/exercise_2/CRUD.js
@@ -1,3 +1,7 @@
 function createUser(data) {
   firebase.firestore().collection("employees").add(data);
 }
+
+// 3 collections: Developer, Test, Manager
+// Fields chung: name, taskList
+// Manager thêm field employeeList
